4bde55bec10345a83f131333ce4b149d40794d1b
Daniel Konishi <dkonishi@gmail.com>
2013-04-24 10:13:57 -0300

    Introduce Braspag::Error to handle all braspag exception easily

--- lib/baby-braspag/errors.rb
+++ lib/baby-braspag/errors.rb
@@ -1,39 +1,40 @@
 module Braspag
-  class InvalidConnection < Exception ; end
-  class InvalidMerchantId < Exception ; end
-  class InvalidConnection < Exception ; end
-  class IncompleteParams < Exception ; end
-  class InvalidOrderId < Exception ; end
-  class InvalidCustomerName < Exception ; end
-  class InvalidCustomerId < Exception ; end
-  class InvalidNumber < Exception ; end
-  class InvalidInstructions < Exception ; end
-  class InvalidExpirationDate < Exception ; end
-  class InvalidStringFormat < Exception ; end
-  class InvalidPost < Exception ; end
-  class InvalidPaymentMethod < Exception ; end
-  class InvalidAmount < Exception ; end
-  class InvalidInstallments < Exception ; end
-  class InvalidHasInterest < Exception ; end
-  class InvalidIP < Exception; end
-  class InvalidCryptKey < Exception; end
-  class InvalidEncryptedKey < Exception; end
-  class InvalidHolder < Exception ; end
-  class InvalidExpirationDate < Exception ; end
-  class InvalidSecurityCode < Exception ; end
-  class InvalidType < Exception ; end
-  class InvalidNumberPayments < Exception ; end
-  class InvalidNumberInstallments < Exception ; end
-  class InvalidJustClickKey < Exception ; end
-  class UnknownError < Exception ; end
+  class Error < Exception; end
+  class InvalidConnection < Error ; end
+  class InvalidMerchantId < Error ; end
+  class InvalidConnection < Error ; end
+  class IncompleteParams < Error ; end
+  class InvalidOrderId < Error ; end
+  class InvalidCustomerName < Error ; end
+  class InvalidCustomerId < Error ; end
+  class InvalidNumber < Error ; end
+  class InvalidInstructions < Error ; end
+  class InvalidExpirationDate < Error ; end
+  class InvalidStringFormat < Error ; end
+  class InvalidPost < Error ; end
+  class InvalidPaymentMethod < Error ; end
+  class InvalidAmount < Error ; end
+  class InvalidInstallments < Error ; end
+  class InvalidHasInterest < Error ; end
+  class InvalidIP < Error; end
+  class InvalidCryptKey < Error; end
+  class InvalidEncryptedKey < Error; end
+  class InvalidHolder < Error ; end
+  class InvalidExpirationDate < Error ; end
+  class InvalidSecurityCode < Error ; end
+  class InvalidType < Error ; end
+  class InvalidNumberPayments < Error ; end
+  class InvalidNumberInstallments < Error ; end
+  class InvalidJustClickKey < Error ; end
+  class UnknownError < Error ; end
 
   class Connection
-    class InvalidMerchantId < Exception ; end
-    class InvalidEnv < Exception ; end
-    class InvalidBraspagUrl < Exception ; end
+    class InvalidMerchantId < Error ; end
+    class InvalidEnv < Error ; end
+    class InvalidBraspagUrl < Error ; end
   end
 
   class Order
-    class InvalidData < Exception; end
+    class InvalidData < Error; end
   end
 end
